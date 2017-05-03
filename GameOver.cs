b232c35fc812575a3e5c46256f873c59933a1006
Goldmato <sam.berrada@gmail.com>
2017-05-02 21:18:35 -0600

    cleaned up a bit and serialized the gameOverDelay field

--- GameOver.cs
+++ GameOver.cs
@@ -1,16 +1,17 @@
 ﻿using System.Collections;
-using System.Collections.Generic;
+
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class GameOver : MonoBehaviour
 {
-	private float gameOverDelay = 3f;
+	// Private serialized fields
+	[SerializeField] private float gameOverDelay = 3f;
 
 	void OnTriggerEnter (Collider other)
 	{
-		// end the game if the player has entered the death zone
-		if (other.gameObject.tag == "PlayerCollider") {
+		// End the game if the player has entered the death zone
+		if (other.gameObject.CompareTag("PlayerCollider")) {
 			StartCoroutine (GameOverMethod ());
 		}
 	}
