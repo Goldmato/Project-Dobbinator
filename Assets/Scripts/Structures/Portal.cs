daf031fc4e238ba7e6a959f5ecbb35b4599150a1
Goldmato <sam.berrada@gmail.com>
2017-05-06 13:40:45 -0600

    initial portal code

---
+++ Assets/Scripts/Structures/Portal.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Collider))]
+public class Portal : MonoBehaviour
+{
+	// Public delegates and events
+	public delegate void PortalEventHandler ();
+	public event PortalEventHandler OnPortalActivated;
+
+	void OnTriggerEnter(Collider other) 
+	{
+		if(other.CompareTag("Player"))
+		{
+			if(OnPortalActivated != null) 
+				OnPortalActivated();
+		}
+	}
+}
