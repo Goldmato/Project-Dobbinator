ac03e9c3d20e36a8ca925aa977d59df05efd614e
Goldmato <sam.berrada@gmail.com>
2017-05-06 09:14:59 -0600

    in the process of moving the repo two directories up...

---
+++ Assets/Scripts/Plugins/PlayerStats.cs
@@ -0,0 +1,39 @@
+﻿/// <summary>
+///	Public container for player stats
+/// </summary>
+[System.Serializable]
+public struct PlayerStats
+{
+	// Fields and properties
+	public float forceMultiplier;
+	public float gravityMultiplier;
+	public float walkSpeed;
+	public float runSpeed;
+	public float jumpSpeed;
+	public float flySpeed;
+
+	/// <summary>
+	/// Shorthand for default values (1, 1, 1, 1, 1, 1)
+	/// </summary>
+	public static PlayerStats Default = new PlayerStats(1, 1, 1, 1, 1, 1);
+
+	// Main constructor 
+	public PlayerStats(float _ForceMult, float _GravMult, float _WalkSpeed, float _RunSpeed, 
+					   float _JumpSpeed, float _FlySpeed)
+	{
+		forceMultiplier = _ForceMult;
+		gravityMultiplier = _GravMult;
+		walkSpeed = _WalkSpeed;
+		runSpeed = _RunSpeed;
+		jumpSpeed = _JumpSpeed;
+		flySpeed  = _FlySpeed;
+	}
+
+	// Operator overload for addition
+	public static PlayerStats operator +(PlayerStats p1, PlayerStats p2)
+	{
+		return new PlayerStats(p1.forceMultiplier + p2.forceMultiplier, p1.gravityMultiplier + p2.gravityMultiplier,
+							   p1.walkSpeed + p2.walkSpeed, p1.runSpeed + p2.runSpeed, p1.jumpSpeed + p2.jumpSpeed,
+							   p1.flySpeed + p2.flySpeed);
+	}
+}
---
+++ Assets/Scripts/Plugins/SimpleInterfaces.cs
@@ -0,0 +1,12 @@
+﻿// This file is just for simple public interfaces
+using UnityEngine;
+
+public interface IInitializable
+{
+	void Initialize();
+}
+
+public interface IPlatform
+{
+	void PlayerBoost(FirstPersonController character);
+}
