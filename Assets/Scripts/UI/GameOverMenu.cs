7e10f09b0044e5581d48ecb81a1739b61fb4ca19
Goldmato <sam.berrada@gmail.com>
2017-05-08 14:02:37 -0600

    game over screen now has a restart button

---
+++ Assets/Scripts/UI/GameOverMenu.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class GameOverMenu : MonoBehaviour 
+{
+	const string MAIN_SCENE = "Main";
+
+	public void OnRestartButtonClicked() 
+	{
+		UnityEngine.SceneManagement.SceneManager.LoadScene(MAIN_SCENE);
+	}
+}
