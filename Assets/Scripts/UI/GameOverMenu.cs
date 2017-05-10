17d6086659f1ebce0e9ea49ed750f5dacfeae40a
Goldmato <sam.berrada@gmail.com>
2017-05-09 23:22:04 -0600

    fixes cursor not being visible

--- Assets/Scripts/UI/GameOverMenu.cs
+++ Assets/Scripts/UI/GameOverMenu.cs
@@ -4,6 +4,13 @@ public class GameOverMenu : MonoBehaviour
 {
 	const string MAIN_SCENE = "Main";
 
+	void Start() 
+	{
+		// Re-enable the cursor 
+		Cursor.lockState = CursorLockMode.None;
+		Cursor.visible = true;
+	}
+
 	public void OnRestartButtonClicked() 
 	{
 		UnityEngine.SceneManagement.SceneManager.LoadScene(MAIN_SCENE);
