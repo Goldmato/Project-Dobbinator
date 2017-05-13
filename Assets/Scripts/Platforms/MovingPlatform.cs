f5bc812f3264af5f9c616333a55806ad506be711
Goldmato <sam.berrada@gmail.com>
2017-05-13 16:47:56 -0600

    simplified to let rigidbody physics do everything

--- Assets/Scripts/Platforms/MovingPlatform.cs
+++ Assets/Scripts/Platforms/MovingPlatform.cs
@@ -14,44 +14,18 @@ public class MovingPlatform : Platform
 
 	Rigidbody m_RigidBody;
 
-	Vector3 m_OriginalPos;
-	Vector3 m_ExtentPos;
-	Vector3 m_TargetPos;
-	Vector3 m_Velocity;
-
 	float m_MoveSpeed;
-	bool  m_Direction;
-	bool  m_RecalculatePath;
 
 	const float MAX_SPEED = 10f;
-	const string ARENA_WALL = "ArenaWall";
 
 	protected override void Start()
 	{
 		base.Start();
 		m_RigidBody = GetComponent<Rigidbody>();
-		m_TargetPos = m_OriginalPos = transform.position;
 		m_MoveSpeed = (MAX_SPEED - Random.Range(m_MoveSpeedLow, m_MoveSpeedHigh)) * GameManager.Current.DifficultyFactor;
 
 		// Rotate platform around the y-axis by a random amount;
 		transform.Rotate(transform.up, Random.Range(0, 360));
-		m_ExtentPos = m_OriginalPos + (transform.forward * m_MoveRange);
-	}
-
-	void FixedUpdate()
-	{
-		if(m_RecalculatePath || Vector3.Distance(transform.position, m_TargetPos) < 0.1f)
-		{
-			m_RecalculatePath = false;
-			m_Direction = !m_Direction;
-			m_TargetPos = m_Direction ? m_ExtentPos : m_OriginalPos;
-			m_RigidBody.velocity = (transform.position - m_TargetPos).normalized * m_MoveSpeed;
-		}
-	}
-
-	void OnCollisionEnter(Collision other)
-	{
-		if(other.gameObject.CompareTag(ARENA_WALL))
-			m_RecalculatePath = true;
+		m_RigidBody.velocity = transform.forward * m_MoveSpeed;
 	}
 }
